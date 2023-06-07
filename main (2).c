1b304b65f66e08d42fe66cd67319166fa12bd87e
douglasribowski <133119060+douglasribowski@users.noreply.github.com>
2023-06-07 07:54:17 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+    int main(){ int contador=0;
+        while(contador<3)
+        {
+            printf("contador=%d\n",contador);
+            contador++;
+        }
+        printf("acabou!!!!");
+    }
