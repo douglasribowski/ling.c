371e81bc9fb23cfef0c7e92ccaf0892ba5c87a9b
douglasribowski <133119060+douglasribowski@users.noreply.github.com>
2023-06-07 08:40:10 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+   int total=0, num;
+    while(total<20)
+    {
+        printf("total:%d\n",total);
+        printf("entre com um número:");
+        scanf("%d",&num);
+        total+=num;
+    }
+    printf("final=%d\n",total);
+} 
